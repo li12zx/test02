81fafabb3e3180aef7b4a05e16a1216f92ea0829
itcast <1609733277@qq.com>
2024-02-15 16:05:29 +0800

    登录功能

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+
+public void loginin(){
+    System.out.println("哈哈哈哈哈");
 }
+}
+
